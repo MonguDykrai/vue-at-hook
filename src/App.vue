325187c85e004bb8d0171ec3d7eb00e195f3753e
jicf <jicf@zylliondata.com>
2019-07-31 09:46:00 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Child @hook:mounted="childMounted" @hook:created="childCreated" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Child from "@/components/Child";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    Child
+  },
+  methods: {
+    childMounted() {
+      console.log(`childMounted`);
+    },
+    childCreated() {
+      console.log(`childCreated`);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="child"></div>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  mounted() {
+    console.log(`child`);
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
